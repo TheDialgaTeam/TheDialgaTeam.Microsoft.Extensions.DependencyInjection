29009209b2ccd682a5edea01795ae62109620e61
Yong Jian Ming <jianming1993@gmail.com>
2019-05-31 05:21:36 +0800

    Allow new stuff

--- src/ServiceCollectionExtensions.cs
+++ src/ServiceCollectionExtensions.cs
@@ -20,5 +20,18 @@ namespace TheDialgaTeam.Microsoft.Extensions.DependencyInjection
 
             return serviceCollection;
         }
+
+        public static IServiceCollection AddInterfacesAndSelfAsSingleton<TService>(this IServiceCollection serviceCollection, TService implementationInstance) where TService : class
+        {
+            if (serviceCollection == null)
+                throw new ArgumentNullException(nameof(serviceCollection));
+
+            serviceCollection.AddSingleton(implementationInstance);
+
+            foreach (var type in typeof(TService).GetInterfaces())
+                serviceCollection.AddSingleton(type, a => a.GetService<TService>());
+
+            return serviceCollection;
+        }
     }
 }
